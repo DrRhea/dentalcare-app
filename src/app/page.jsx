5450686142f0ddfc8092c6fcf4257638a41357d1
Arya Jagadditha <czajr13@gmail.com>
2025-11-15 21:46:26 +0700

    update: ganti semua video YouTube dengan video edukasi kesehatan gigi

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -48,8 +48,8 @@ export default function Home() {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/9iEPzbG-xLE?rel=0&modestbranding=1"
-              title="Cara Merawat Kesehatan Gigi dan Mulut"
+              src="https://www.youtube.com/embed/BaUnfIaI1rE?rel=0&modestbranding=1"
+              title="Video Edukasi Kesehatan Gigi"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
